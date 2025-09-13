50fd69a2537a72c611cff9e0eda41a4d27b246e7
Mohammad Aghili <mhaghili1381@yahoo.com>
2025-09-13 19:32:23 +0330

    make X/O more intelligence

--- src/gamification-module/components/TicTacToe.js
+++ src/gamification-module/components/TicTacToe.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
-// --- Styled Components for the Game Board ---
+
 const Board = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
@@ -21,7 +21,6 @@ const Cell = styled.div`
   font-size: 36px;
   cursor: pointer;
   background-color: #f0f0f0;
-
   &:hover {
     background-color: #e0e0e0;
   }
@@ -31,16 +30,13 @@ const TicTacToe = (props) => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [isPlayerTurn, setIsPlayerTurn] = useState(true);
 
-  // Check for winner on every board change
   useEffect(() => {
     const winner = calculateWinner(board);
     if (winner) {
       props.actionProvider.handleGameResult({ winner });
     } else if (!board.includes(null)) {
-      // It's a draw
       props.actionProvider.handleGameResult({ winner: "Draw" });
     } else if (!isPlayerTurn) {
-      // Bot's turn
       setTimeout(botMove, 500);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -55,16 +51,77 @@ const TicTacToe = (props) => {
     }
   };
 
+  // --- Complete rewrite of the botMove function with new logic ---
   const botMove = () => {
-    const emptyCells = board
-      .map((cell, index) => (cell === null ? index : null))
-      .filter((val) => val !== null);
+    let bestMove = -1;
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-    if (emptyCells.length > 0) {
-      const randomIndex =
-        emptyCells[Math.floor(Math.random() * emptyCells.length)];
+    // 1. First priority: Find a winning move for the bot ('O')
+    for (const line of lines) {
+      const [a, b, c] = line;
+      if (board[a] === "O" && board[a] === board[b] && board[c] === null)
+        bestMove = c;
+      if (board[a] === "O" && board[a] === board[c] && board[b] === null)
+        bestMove = b;
+      if (board[b] === "O" && board[b] === board[c] && board[a] === null)
+        bestMove = a;
+      if (bestMove !== -1) break;
+    }
+
+    // 2. Second priority: Block the player's ('X') winning move
+    if (bestMove === -1) {
+      for (const line of lines) {
+        const [a, b, c] = line;
+        if (board[a] === "X" && board[a] === board[b] && board[c] === null)
+          bestMove = c;
+        if (board[a] === "X" && board[a] === board[c] && board[b] === null)
+          bestMove = b;
+        if (board[b] === "X" && board[b] === board[c] && board[a] === null)
+          bestMove = a;
+        if (bestMove !== -1) break;
+      }
+    }
+
+    // If no strategic move was found, proceed to the next priorities
+    if (bestMove === -1) {
+      const emptyCells = board
+        .map((cell, index) => (cell === null ? index : null))
+        .filter((val) => val !== null);
+
+      // 3. Third priority: Take the center
+      if (emptyCells.includes(4)) {
+        bestMove = 4;
+      } else {
+        // 4. Fourth priority: Take a corner
+        const corners = [0, 2, 6, 8].filter((index) =>
+          emptyCells.includes(index)
+        );
+        if (corners.length > 0) {
+          bestMove = corners[Math.floor(Math.random() * corners.length)];
+        } else {
+          // 5. Fifth priority: Take a side
+          const sides = [1, 3, 5, 7].filter((index) =>
+            emptyCells.includes(index)
+          );
+          if (sides.length > 0) {
+            bestMove = sides[Math.floor(Math.random() * sides.length)];
+          }
+        }
+      }
+    }
+
+    if (bestMove !== -1) {
       const newBoard = [...board];
-      newBoard[randomIndex] = "O";
+      newBoard[bestMove] = "O";
       setBoard(newBoard);
       setIsPlayerTurn(true);
     }
