27801dfd0044d349752d7add844acefafd63081c
MhAghili <mhaghili1381@yahoo.com>
2025-09-20 12:56:19 +0330

    fix

--- src/chatbot/ActionProvider.js
+++ src/chatbot/ActionProvider.js
@@ -255,7 +255,6 @@ class ActionProvider {
   };
 
   handleGameAnswer = (userAnswer) => {
-    debugger;
     const { game, endGame, addPoints, incrementQuestionsAsked } =
       gamificationAPI.useStore.getState();
 
